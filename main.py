5f0024f03c68f04c0cd2218fb016f51759e0a360
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-05-29 10:57:24 -0400

    moving ball

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import sys, pygame
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 WHITE = (255, 255, 255)
-Black = (200,200,0)
+Black = (0,0,0)
+speed = [1,1]
 
 
 BALL_WIDTH, BALL_HEIGHT = 55, 55
 FPS = 60
 
-speed = [1,1]
+
 blue = 0 , 0, 255
 # barpositionbegin = 200
 # barwidth = 100
@@ -22,24 +23,53 @@ blue = 0 , 0, 255
 VEL = 5
 
 ballImage = pygame.image.load(os.path.join('Assets', 'ball.png'))
-ball = pygame.transform.rotate(pygame.transform.scale(ballImage, (BALL_WIDTH, BALL_HEIGHT)), 90)
-ballrect = ball.get_rect()
+ball = pygame.transform.scale(ballImage, (BALL_WIDTH, BALL_HEIGHT))
 
 bar = pygame.Surface((200,50)) 
-# pygame.draw.rect(bar,blue,(0,0,100,50))
+barr = pygame.draw.rect(bar,blue,(0,0,200,50))
+
+ballrect = ball.get_rect()
 
 
-def draw_window():
+def draw_window(ballPosition):
     WIN.fill(Black)
-    WIN.blit(ball, ballrect)
-    WIN.blit(bar,(200,10)) 
+    WIN.blit(ball, (ballPosition.x, ballPosition.y))
+    WIN.blit(bar,(200,HEIGHT - 50)) 
+    
+    # ballrectt = ballrect.move(speed)
+    # ballposition = ballrectt.left
+    # if ballrectt.left < 0 or ballrectt.right > WIDTH:
+    #     speed[0] = -speed[0]
+    # if ballrectt.top < 0 or ballrectt.bottom > HEIGHT:
+    #     speed[1] = -speed[1]
+    
+    
     pygame.display.update()
     
+def moveBall(ballPosition):
+    
+    ballPosition.x += speed[0]
+    ballPosition.y += speed[1]
+    
+    if ballPosition.left < 0 or ballPosition.right > WIDTH:
+        speed[0] = -speed[0]
+    if ballPosition.top < 0 or ballPosition.bottom > HEIGHT:
+        speed[1] = -speed[1]
+    
+    
+    
+    
+
+
+
 
 
 pygame.display.set_caption("First Game!")
 
 def main():
+    
+    ballPosition = pygame.Rect(100,300,50,50)
+    
     clock = pygame.time.Clock()
     
     run = True
@@ -49,7 +79,15 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window()
+        
+        moveBall(ballPosition)
+        
+        
+        draw_window(ballPosition)
+        
+        
+        
+        
 
         
         
@@ -86,11 +124,11 @@ if  __name__ == "__main__":
 
 
 
-# def bar_handle_movement(keys_pressed, barpositionbegin, VEL, width, barwidth):
-#     count = bluebar.x
-#     if keys_pressed[pygame.K_SPACE]: 
+# # def bar_handle_movement(keys_pressed, barpositionbegin, VEL, width, barwidth):
+# #     count = bluebar.x
+# #     if keys_pressed[pygame.K_SPACE]: 
         
-#         print(count)
+# #         print(count)
         
         
         
@@ -128,8 +166,8 @@ if  __name__ == "__main__":
 
 # while 1:
   
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT: sys.exit()
+#     # for event in pygame.event.get():
+#     #     if event.type == pygame.QUIT: sys.exit()
 
 
 #     ballrect = ballrect.move(speed)
@@ -148,11 +186,8 @@ if  __name__ == "__main__":
         
 #     # ballposition = ballrect.left
     
-   
         
     
-
-  
     
     
 #     # print(bluebar.x)
@@ -160,17 +195,15 @@ if  __name__ == "__main__":
 #     # print(bluebar.x)
     
     
-#     keys_pressed = pygame.key.get_pressed()
-
+#     # keys_pressed = pygame.key.get_pressed()
     
-#     bar_handle_movement(keys_pressed, barpositionbegin, VEL, width, barwidth)
+#     # bar_handle_movement(keys_pressed, barpositionbegin, VEL, width, barwidth)
         
     
     
    
     
 #     screen.blit(ball, ballrect)
-  
 #     pygame.display.flip()
 
 
